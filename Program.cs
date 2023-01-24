066edb1146826a9aa91188ed712cbfcb28304872
makar <makar.t1@mail.ru>
2023-01-24 20:30:27 +0300

    programm in cycle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿string[] massive = {"hello", "2", "world", ":-)"};
 for (int i = 0; i < massive.Length; i++)
 {
-
+    bool flag = false;
+    if (massive[i].Length <4)
+    {
+        flag = true;
+    }
+    if (flag == true)
+    {
+        System.Console.WriteLine(massive[i]);
+    }
 }
